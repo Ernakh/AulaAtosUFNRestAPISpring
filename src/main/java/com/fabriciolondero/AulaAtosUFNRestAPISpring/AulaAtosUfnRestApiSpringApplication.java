523bd93482460185ff3d77122d4aa88e4cb7aae8
Ernakh <fabriciotonettolondero@gmail.com>
2021-04-06 14:24:56 -0300

    primeiro commit

---
+++ src/main/java/com/fabriciolondero/AulaAtosUFNRestAPISpring/AulaAtosUfnRestApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fabriciolondero.AulaAtosUFNRestAPISpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaAtosUfnRestApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaAtosUfnRestApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabriciolondero/AulaAtosUFNRestAPISpring/AulaAtosUfnRestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciolondero.AulaAtosUFNRestAPISpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaAtosUfnRestApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
